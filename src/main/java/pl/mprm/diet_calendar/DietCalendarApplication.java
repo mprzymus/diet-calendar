571efa877a302c3812a2e68e89faad983d841b2d
mprzymus <przymus@gmail.com>
2020-12-30 23:28:28 +0100

    start

---
+++ src/main/java/pl/mprm/diet_calendar/DietCalendarApplication.java
@@ -0,0 +1,13 @@
+package pl.mprm.diet_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mprm/diet_calendar/DietCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mprm.diet_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
