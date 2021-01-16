0b112419e3951344a392183b7e3f996946ae2738
mprzymus <przymus@gmail.com>
2021-01-16 16:32:12 +0100

    Bug fixed: invalid security config

--- src/main/java/pl/mprm/diet_calendar/controllers/CalculatorController.java
+++ src/main/java/pl/mprm/diet_calendar/controllers/CalculatorController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CalculatorController {
-    @GetMapping("/calc")
+    @GetMapping("/open/calc")
     public String dietCalculator() {
         return "calcform";
     }
