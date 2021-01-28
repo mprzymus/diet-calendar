845e63e894ca3280bd0e8d81f32631983cc39a39
mprzymus <przymus@gmail.com>
2021-01-29 00:00:48 +0100

    Security changed

--- src/main/java/pl/mprm/diet_calendar/configurations/SecurityConfiguration.java
+++ src/main/java/pl/mprm/diet_calendar/configurations/SecurityConfiguration.java
@@ -36,7 +36,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/index").permitAll()
                 .antMatchers("/open/**").permitAll()
-                .antMatchers("/patient/**").hasAnyRole(PATIENT_USER_ROLE, DIETITIAN_USER_ROLE)
+                .antMatchers("/patient/**").hasAnyRole(PATIENT_USER_ROLE)
                 .antMatchers("/dietitian/**").hasRole(DIETITIAN_USER_ROLE)
                 .antMatchers("/h2-console/**").permitAll()
                 .and().csrf().ignoringAntMatchers("/h2-console/**")
