389745e19b3bff74b71c4bd03019b1dbf2c6e580
mprzymus <przymus@gmail.com>
2021-01-29 19:26:45 +0100

    refactoring

--- src/main/resources/static/js/calculator.js
+++ src/main/resources/static/js/calculator.js
@@ -1,15 +1,14 @@
 function calculate(mass, height, age, sex) {
-    var mass = document.getElementById(mass).value;
+    mass = document.getElementById(mass).value;
     console.log(mass);
-    var height = document.getElementById(height).value;
+    height = document.getElementById(height).value;
     console.log(height);
-    var age = document.getElementById(age).value;
+    age = document.getElementById(age).value;
     console.log(age);
-    var sex = document.getElementById(sex).value;
+    sex = document.getElementById(sex).value;
     console.log(sex);
-    var result = 9.99*mass + 6.25*height - 4.92*age;
-    //(9,99 x masa ciała) + (6,25 x wzrost) – (4,92 x wiek) - 161/+5
-    if(sex != "male"){
+    let result = 9.99 * mass + 6.25 * height - 4.92 * age;
+    if(sex !== "male"){
         result -= 161;
     } else{
         result += 5;
